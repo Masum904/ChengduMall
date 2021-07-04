494ff6c5d64dc1d982a65cd7b5cc0c97b030f9d1
Md Masum Billal <billalmasum432@gmail.com>
2021-07-05 02:35:33 +0800

    myfile

--- app/src/main/java/com/example/chengdumall/MainActivity.java
+++ app/src/main/java/com/example/chengdumall/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.chengdumall;
 
 import android.app.Activity;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
+    private Button button;
+    String string="new String";
 }
